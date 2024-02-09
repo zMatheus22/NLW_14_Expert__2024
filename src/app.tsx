32dd2986b1888875b757e8f991973e00b77419db
Matheus Vinicyus Strada <57578830+zMatheus22@users.noreply.github.com>
2024-02-09 15:16:45 -0300

    Implementing configuration for the `new Notes card` using `useState`

--- src/app.tsx
+++ src/app.tsx
@@ -1,13 +1,27 @@
+import { useState } from "react";
 import logo from "./asset/logo-nlw-expert.svg";
 import { NewNoteCard } from "./components/new-note-card";
 import { NoteCard } from "./components/note-card";
 
-const note = {
-  date: new Date(),
-  content: "Olá",
-};
+interface Note {
+  id: string;
+  date: Date;
+  content: string;
+}
 
 export function App() {
+  const [notes, setNotes] = useState<Note[]>([]);
+
+  function onNoteCreated(content: string) {
+    const newNote = {
+      id: crypto.randomUUID(),
+      date: new Date(),
+      content,
+    };
+
+    setNotes([newNote, ...notes]);
+  }
+
   return (
     <div className="mx-auto max-w-6xl my-12 space-y-6">
       <img src={logo} alt="NLW Expert" />
@@ -23,8 +37,10 @@ export function App() {
       <div className="h-px bg-slate-700" />
 
       <div className="grid grid-cols-3 gap-6 auto-rows-[250px]">
-        <NewNoteCard />
-        <NoteCard note={note} />
+        <NewNoteCard onNoteCreated={onNoteCreated} />
+        {notes.map((note) => {
+          return <NoteCard key={note.id} note={note} />;
+        })}
       </div>
     </div>
   );
