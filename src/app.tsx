d41839d30212ab8b066ce7511cedf7d20dc57b01
Matheus Vinicyus Strada <57578830+zMatheus22@users.noreply.github.com>
2024-02-09 16:25:58 -0300

    Responsividade mobile

--- src/app.tsx
+++ src/app.tsx
@@ -48,7 +48,7 @@ export function App() {
       : notes;
 
   return (
-    <div className="mx-auto max-w-6xl my-12 space-y-6">
+    <div className="mx-auto max-w-6xl my-12 space-y-6 px-5">
       <img src={logo} alt="NLW Expert" />
       <form className="w-full">
         <input
@@ -62,7 +62,7 @@ export function App() {
       {/* Linha horizontal */}
       <div className="h-px bg-slate-700" />
 
-      <div className="grid grid-cols-3 gap-6 auto-rows-[250px]">
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 auto-rows-[250px]">
         <NewNoteCard onNoteCreated={onNoteCreated} />
         {filteredNotes.map((note) => {
           return <NoteCard key={note.id} note={note} />;
